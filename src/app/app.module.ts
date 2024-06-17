323a18f5172611985525fa95db5963fd7c5e2096
manuel1993 <manuel.sperati@yahoo.com>
2024-06-17 13:02:07 +0200

    output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodoListComponent, TodoItemComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Todo } from '../../models/todo';
+import { TODOS } from '../../data/todos';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css',
+})
+export class TodoListComponent {
+  todos: Todo[] = TODOS;
+
+  completa(todo: Todo) {
+    console.log('Richiesta eliminazione:', todo);
+  }
+}
